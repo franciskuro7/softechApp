e0930dcd07429c918ec53760a97032b6b1874594
franciskuro7git commit -m push <franciskuro7.com>
2024-06-03 14:35:17 +0100

    first time pushing

---
+++ front-end/script/script.js
@@ -0,0 +1,7 @@
+const signIn = document.getElementById('signin')
+const sideBar = document.getElementById('mobile_nav-bar')
+// const sideBar = document.getElementById('mobile_nav-bar')
+
+signIn.addEventListener('click', ()=>{
+    alert('hello world')
+})
